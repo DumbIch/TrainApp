eeab2ba5f231e3c1a953590c82a87b366887be43
DumDumbIch <d.lebedev2@eapteka.ru>
2024-02-04 13:36:07 +0300

    Initial commit

---
+++ app/src/main/java/ru/dumdumbich/trainapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.dumdumbich.trainapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.dumdumbich.trainapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var ui: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ui = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+
+    }
+}
